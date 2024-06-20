00a4d3a7b0a69f074e014d232b28206eeb9b9a7b
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-06-19 21:48:14 -0500

    Logic Paciente

--- paciente/logic/logicPaciente.py
+++ paciente/logic/logicPaciente.py
@@ -1,10 +1,31 @@
 from ..models import Paciente
 
-def getPacientes():
-    return Paciente.objects.all().order_by('name')
-
 def createPaciente(data):
     try:
         return Paciente.objects.create(data)
     except: 
-        raise Exception({'error':'Error al crear Paciente'})
+        raise Exception({'error':'Error al crear Paciente'},404)
+
+def getPacientes():
+    return Paciente.objects.all().order_by('name')
+
+def getPacienteByDocuemnt(docuemnt):
+    try:
+        return Paciente.objects.get(docuemnt=docuemnt)
+    except:
+        raise Exception({'error':'Hubo un error, el Paciente no existe'},404)
+
+def deletePacienteByDocument(document):
+    try:
+        Paciente.objects.get(document=document).delete()
+    except:
+        raise Exception({'error':'No fue posible borrar el Paciente'},404)
+
+def updatePacienteByDocument(data):
+    try:
+        paciente = Paciente.objects.get(data['document'])
+        setattr(paciente, data)
+        paciente.save()
+        return paciente
+    except:
+        raise Exception({'error':'No fue posible actualizar el Paciente'},404)
