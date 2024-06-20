9d5c02a0a7da4042cbdff0e3a423836200215c9b
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-06-19 22:58:08 -0500

    Fin de paciente y correccion citas models

--- citas/models.py
+++ citas/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+import sys
+sys.path.append("..")
+from paciente.models import Paciente
 
 class Citas(models.Model):
-    paciente = models.ForeignKey(null=False, blank=False)
+    paciente = models.ForeignKey(Paciente, null=False, blank=False, on_delete=models.CASCADE)
     date = models.DateField(null=False, blank=False, default=None)
     reason = models.TextField()
     treatment = models.TextField()
-    examsData = JSONField(default=dict)
+    examsData = models.JSONField(default=dict)
 
